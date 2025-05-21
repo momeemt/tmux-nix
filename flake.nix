064977034f7d0b29c641b839d2f6e60bd67125a0
Mutsuha Asada <me@momee.mt>
2025-05-21 16:18:59 +0900

    Refactor test script with common check function

--- flake.nix
+++ flake.nix
@@ -83,11 +83,23 @@
               home.stateVersion = "24.11";
             };
           };
-          testScript = ''
-            start_all
-            machine.wait_for_unit("default.target");
-            machine.succeed("su - alice -c 'tmux -V'");
-          '';
+          testScript =
+            let
+              tmuxConf = "/home/alice/.tmux.conf";
+              check = line:
+                "machine.succeed(\"grep -q '^${line}$' ${tmuxConf}\")";
+            in
+            ''
+              start_all
+              machine.wait_for_unit("default.target");
+              machine.succeed("su - alice -c 'tmux -V'");
+              ${check "set-option -g prefix C-a"}
+              ${check "bind-key h select-pane -L"}
+              ${check "bind-key l select-pane -R"}
+              ${check "bind-key j select-pane -U"}
+              ${check "bind-key k select-pane -D"}
+              ${check "display-message \"Hello, tmux-nix!\""}
+            '';
         };
 
         treefmt = {
