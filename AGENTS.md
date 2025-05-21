30721760cf4fb3084bbd831c824e0ea693637829
Mutsuha Asada <me@momee.mt>
2025-05-21 16:32:42 +0900

    doc: define agent commit authorship
    Co-authored-by: Mutsuha Asada <me@momee.mt>

---
+++ AGENTS.md
@@ -0,0 +1,5 @@
+# Codex Agent Instructions
+
+All commits must use `Codex <codex@openai.com>` as the author.
+Include the following co-author line at the end of every commit message:
+`Co-authored-by: Mutsuha Asada <me@momee.mt>`
--- flake.nix
+++ flake.nix
@@ -83,10 +83,18 @@
               home.stateVersion = "24.11";
             };
           };
-          testScript = ''
+          testScript = let
+            check = line: "machine.succeed(\"grep -q '^${line}$' /home/alice/.tmux.conf\")";
+          in ''
             start_all
             machine.wait_for_unit("default.target");
             machine.succeed("su - alice -c 'tmux -V'");
+            ${check "set-option -g prefix C-a"}
+            ${check "bind-key h select-pane -L"}
+            ${check "bind-key l select-pane -R"}
+            ${check "bind-key j select-pane -U"}
+            ${check "bind-key k select-pane -D"}
+            ${check "display-message \"Hello, tmux-nix!\""}
           '';
         };
 
