ff62d3343659c9b442d4d2b8a14aac0315b10e41
Mutsuha Asada <me@momee.mt>
2025-05-23 16:33:18 +0900

    refactor: tests

--- flake.nix
+++ flake.nix
@@ -56,7 +56,11 @@
 
         checks.tmux-nix-test = pkgs.nixosTest {
           name = "tmux-nix-test";
-          nodes.machine = {pkgs, ...}: {
+          nodes.machine = {
+            pkgs,
+            lib,
+            ...
+          }: {
             imports = [
               inputs.home-manager.nixosModules.home-manager
             ];
@@ -64,88 +68,22 @@
             home-manager.users.alice = {
               imports = [
                 (import ./modules)
+                (import ./tests/inputs/default.nix)
+                (import ./tests/inputs/plugins/cpu.nix)
               ];
-              tmux-nix = {
-                enable = true;
-                prefix = "C-a";
-                keymaps = {
-                  pane = {
-                    left.key = "h";
-                    right.key = "l";
-                    up = {
-                      key = "j";
-                      repeatable = true;
-                    };
-                    down = {
-                      key = "k";
-                      repeatable = true;
-                    };
-                  };
-                  resize = {
-                    left = {
-                      key = "H";
-                      amount = 10;
-                    };
-                    right = {
-                      key = "L";
-                      amount = 10;
-                    };
-                    up = {
-                      key = "J";
-                      repeatable = true;
-                      amount = 10;
-                    };
-                    down = {
-                      key = "K";
-                      repeatable = true;
-                      amount = 10;
-                    };
-                  };
-                };
-                plugins.cpu = {
-                  enable = true;
-                  cpu.lowIcon = "-";
-                  cpu.mediumThresh = 20;
-                  cpu.highThresh = 80;
-                  ram.mediumBgColor = "#[bg=yellow]";
-                  gpu.lowIcon = "G";
-                  gram.percentageFormat = "%5.1f%%";
-                  cpuTemp.unit = "F";
-                  gpuTemp.unit = "F";
-                };
-                extraConfig = ''
-                  display-message "Hello, tmux-nix!"
-                '';
-              };
               home.stateVersion = "24.11";
             };
           };
           testScript = let
-            escape = s: pkgs.lib.strings.escape ["\"" "[" "]"] s;
-            check = line: "machine.succeed(\"grep -q '^${escape line}$' /home/alice/.tmux.conf\")";
+            testLib = import ./tests/lib.nix {inherit pkgs;};
+            rootChecks = import ./tests/outputs {inherit pkgs testLib;};
+            cpuChecks = import ./tests/outputs/plugins/cpu.nix {inherit pkgs testLib;};
           in ''
             start_all
             machine.wait_for_unit("default.target");
             machine.succeed("su - alice -c 'tmux -V'");
-            ${check "set-option -g prefix C-a"}
-            ${check "bind-key h select-pane -L"}
-            ${check "bind-key l select-pane -R"}
-            ${check "bind-key -r j select-pane -U"}
-            ${check "bind-key -r k select-pane -D"}
-            ${check "bind-key H resize-pane -L 10"}
-            ${check "bind-key L resize-pane -R 10"}
-            ${check "bind-key -r J resize-pane -U 10"}
-            ${check "bind-key -r K resize-pane -D 10"}
-            ${check "display-message \"Hello, tmux-nix!\""}
-            ${check "run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/tmux-cpu/cpu.tmux"}
-            ${check "set -g @cpu_low_icon \"-\""}
-            ${check "set -g @cpu_medium_thresh \"20\""}
-            ${check "set -g @cpu_high_thresh \"80\""}
-            ${check "set -g @ram_medium_bg_color \"#[bg=yellow]\""}
-            ${check "set -g @gpu_low_icon \"G\""}
-            ${check "set -g @gram_percentage_format \"%5.1f%%\""}
-            ${check "set -g @cpu_temp_unit \"F\""}
-            ${check "set -g @gpu_temp_unit \"F\""}
+            ${rootChecks}
+            ${cpuChecks}
           '';
         };
 
